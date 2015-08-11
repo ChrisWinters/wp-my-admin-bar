016e4437bafeb3efdd9f3de5db2d5aecd5d331e5
tribalNerd <tribalnerd@technerdia.com>
2015-08-11 14:36:01 -0400

    Version Update

--- wp-my-admin-bar.php
+++ wp-my-admin-bar.php
@@ -1,6 +1,6 @@
 <?php
-ini_set("display_errors", "1");
-error_reporting(E_ALL | E_STRICT);
+#ini_set("display_errors", "1");
+#error_reporting(E_ALL | E_STRICT);
 /**
  * Plugin Name: WP My Admin Bar | Admin Bar
  * Plugin URI: http://technerdia.com/projects/adminbar/plugin.html
